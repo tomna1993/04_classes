2f12733caaa52f8d8a9fbd524e46d0ba228304c3
TomasNagy <tomi.nagy@outlook.com>
2023-08-10 15:09:25 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using _04_classes;
+using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main()
+        {
+            Book book1 = new Book();
+
+            book1.title = "Test Title";
+            book1.author = "Test Author";
+            book1.pages = 345;
+
+            Console.WriteLine(book1.title);
+
+            Console.ReadLine();
+        }
+    }
+}
